55b72c21e52a3d2a190bfde15fffb4bfd1c91e05
1Laggy1 <andryushka05@gmail.com>
2025-09-17 21:38:36 +0300

    Ball added
    - Created class STUBall, with MeshComponent.
    - The only function for STUBall for now is to get Material and Point Light. Colors will be changed depending on the team carying it.
    - Later here will be also checking collision with character on server, and if there was a collision, character will pick it up.

---
+++ Source/ShootThemUp/Private/Resources/STUBall.cpp
@@ -0,0 +1,31 @@
+﻿// Shoot THem Up Game. All Rights Reserved.
+
+
+#include "Resources/STUBall.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/PointLightComponent.h"
+#include "UObject/ConstructorHelpers.h"
+
+ASTUBall::ASTUBall()
+{
+	PrimaryActorTick.bCanEverTick = false;
+    MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+    RootComponent = MeshComponent;
+    PointLight = CreateDefaultSubobject<UPointLightComponent>(TEXT("PointLight"));
+    PointLight->SetupAttachment(RootComponent);
+}
+
+void ASTUBall::BeginPlay()
+{
+    Super::BeginPlay();
+
+    if (MeshComponent)
+    {
+        CurrentMaterial = MeshComponent->GetMaterial(0);
+
+        if (CurrentMaterial)
+        {
+            UE_LOG(LogTemp, Warning, TEXT("Material found: %s"), *CurrentMaterial->GetName());
+        }
+    }
+}
---
+++ Source/ShootThemUp/Public/Resources/STUBall.h
@@ -0,0 +1,31 @@
+﻿// Shoot THem Up Game. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "STUBall.generated.h"
+
+class UStaticMeshComponent;
+class UPointLightComponent;
+class UMaterialInterface;
+
+UCLASS()
+class SHOOTTHEMUP_API ASTUBall : public AActor
+{
+	GENERATED_BODY()
+
+  public:
+    ASTUBall();
+
+  protected:
+    UPROPERTY(VisibleAnywhere)
+    UStaticMeshComponent *MeshComponent;
+
+    UPROPERTY(VisibleAnywhere)
+    UPointLightComponent *PointLight;
+
+    UMaterialInterface *CurrentMaterial;
+    virtual void BeginPlay() override;
+
+};
